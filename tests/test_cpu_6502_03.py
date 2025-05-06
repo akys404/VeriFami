607e46a3b8dc1225063118ed8612a80ca057f080
akys404 <autumnbetter@gmail.com>
2025-05-06 23:09:30 +0900

    テストベンチのフォルダ構成を変更 #2

--- tests/test_cpu_6502_03.py
+++ tests/test_cpu_6502_03.py
@@ -151,6 +151,300 @@ def test_sample():
     u_cpu.r_x = 0xFE
     assert u_cpu.r_x == 0xFE
 
+#60 CMP-ABS,Y[=D9h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#61 CPX-IMMEDIATE[=E0h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#62 CPX-ABSOLUTE[=ECh]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#63 CPX-ZEROPAGE[=E4h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#64 CPY-IMMEDIATE[=C0h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#65 CPY-ABSOLUTE[=CCh]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#66 CPY-ZEROPAGE[=C4h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#67 EOR-IMMEDIATE[=49h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#68 EOR-ABSOLUTE[=4Dh]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#69 EOR-ZEROPAGE[=45h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#70 EOR-(IND,X)[=41h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#71 EOR-(IND),Y[=51h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#72 EOR-ZPG,X[=55h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#73 EOR-ABS,X[=5Dh]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#74 EOR-ABS,Y[=59h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#75 LDA-IMMEDIATE[=A9h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#76 LDA-ABSOLUTE[=ADh]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#77 LDA-ZEROPAGE[=A5h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#78 LDA-(IND,X)[=A1h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#79 LDA-(IND),Y[=B1h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#80 LDA-ZPG,X[=B5h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#81 LDA-ABS,X[=BDh]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#82 LDA-ABS,Y[=B9h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#83 LDX-IMMEDIATE[=A2h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#84 LDX-ABSOLUTE[=AEh]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#85 LDX-ZEROPAGE[=A6h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#86 LDX-ABS,Y[=BEh]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#87 LDX-ZPG,Y[=B6h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#88 LDY-IMMEDIATE[=A0h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#89 LDY-ABSOLUTE[=ACh]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#90 LDY-ZEROPAGE[=A4h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#91 LDY-ZPG,X[=B4h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#92 LDY-ABS,X[=BCh]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#93 ORA-IMMEDIATE[=09h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#94 ORA-ABSOLUTE[=0Dh]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#95 ORA-ZEROPAGE[=05h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#96 ORA-(IND,X)[=01h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#97 ORA-(IND),Y[=11h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#98 ORA-ZPG,X[=15h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#99 ORA-ABS,X[=1Dh]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#100 ORA-ABS,Y[=19h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#101 SBC-IMMEDIATE[=E9h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#102 SBC-ABSOLUTE[=EDh]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#103 SBC-ZEROPAGE[=E5h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#104 SBC-(IND,X)[=E1h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#105 SBC-(IND),Y[=F1h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#106 SBC-ZPG,X[=F5h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#107 SBC-ABS,X[=FDh]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
+#108 SBC-ABS,Y[=F9h]
+def test_sample():
+    u_cpu = mod_cpu(debug=False)
+    u_cpu.r_x = 0xFE
+    assert u_cpu.r_x == 0xFE
+
 
 if __name__ == "__main__":
     test_sample()
